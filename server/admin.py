07759566b39d2648c1fd149495790e86a28d4b6b
Jeremy Parker <github@jeremyparker.org>
2015-03-06 16:14:16 -0500

    testing with a trivial change

--- server/admin.py
+++ server/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 import models as server_models
 
-admin.site.register(server_models.Driver)
 admin.site.register(server_models.FleetPartner)
+admin.site.register(server_models.Driver)
