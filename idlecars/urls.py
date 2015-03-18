7c2cb23deff2a5680c2fbcb7a340acc1cdc0f1e1
jefk <speedracer111@gmail.com>
2015-03-17 20:01:07 -0400

    send a response

--- idlecars/urls.py
+++ idlecars/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from server import views
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'idlecars.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', views.index, name='index'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- server/views.py
+++ server/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import JsonResponse
+
+def index(request):
+    return JsonResponse({'cars': 'idle'})
