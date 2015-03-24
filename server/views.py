81c121fc30d3cfb21d3b6c0f93e3f5ed7bc09e63
jefk <speedracer111@gmail.com>
2015-03-24 11:33:55 -0400

    add path to send an email

--- server/views.py
+++ server/views.py
@@ -4,6 +4,10 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 from django.http import JsonResponse
+from django.http import HttpResponse
 
 def index(request):
     return JsonResponse({'cars': 'idle'})
+
+def email(request):
+    return HttpResponse("<h1>I'm trying to send an email</h1>")
