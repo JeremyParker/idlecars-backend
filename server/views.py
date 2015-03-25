0ffc63e10a8a55c94d702cfa8aceb9781b89822b
jefk <speedracer111@gmail.com>
2015-03-25 12:25:23 -0400

    use jade templates

--- server/views.py
+++ server/views.py
@@ -2,12 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
 from django.http import JsonResponse
-from django.http import HttpResponse
+
+from idlecars.job_queue import job_queue
 
 from server import jobs
-from idlecars.job_queue import job_queue
 
 def index(request):
     return JsonResponse({'cars': 'idle'})
@@ -20,6 +19,6 @@ def email(request):
         job_queue.enqueue(jobs.send_welcome_email, email_address())
 
         text = 'Im trying to send an email to {}'.format(email_address())
-        return HttpResponse('<h1>{text}</h1>'.format(**locals()))
+        return render(request, 'email.jade', { 'email_address': email_address() })
     else:
-        return HttpResponse("<h1>Add an address param please!</h1>")
+        return render(request, 'no_email.jade')
