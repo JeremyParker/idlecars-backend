fc97a60337550c3918941e88b0c4288f9cf6c6fd
Jeremy Parker <github@jeremyparker.org>
2015-03-26 16:33:52 -0400

    welcome email hooked up to landing page form submission.

--- server/jobs.py
+++
@@ -1,7 +0,0 @@
-# -*- encoding:utf-8 -*-
-from __future__ import unicode_literals
-
-from idlecars.email import SendgridEmail
-
-def send_welcome_email(to_address):
-    return SendgridEmail().send_to(to_address)
