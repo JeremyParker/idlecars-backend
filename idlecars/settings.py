cb2300e4dcba728e29e1ec0046c338928d9a0e05
jefk <speedracer111@gmail.com>
2015-03-26 13:37:42 -0400

    swap names of content and container

--- idlecars/settings.py
+++ idlecars/settings.py
@@ -93,6 +93,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+IMAGE_URL = '/static/img/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
