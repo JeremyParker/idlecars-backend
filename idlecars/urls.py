936cbe87283cb43dc6e782ce60d52b89eae00250
jefk <speedracer111@gmail.com>
2015-04-08 16:00:17 -0400

    clean up namespace

--- idlecars/urls.py
+++ idlecars/urls.py
@@ -4,13 +4,13 @@ from __future__ import unicode_literals
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from server import views
+import server.views
 import web_app.urls
 
 urlpatterns = patterns('',
     url(r'', include('website.urls', namespace='website')),
 
-    url(r'^api/$', views.index, name='index'),
+    url(r'^api/$', server.views.index, name='index'),
 
     url(r'^app/', include(web_app.urls)),
 
