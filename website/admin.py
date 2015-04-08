6f32934037e1989d0e145d62c745a964d1a79f24
Jeremy Parker <github@jeremyparker.org>
2015-04-08 09:59:22 -0400

    the Contact admin shows all fields in listing and read-only fields in the details view.

--- website/admin.py
+++ website/admin.py
@@ -5,6 +5,27 @@ from django.contrib import admin
 
 import models
 
-admin.site.register(models.Contact)
-admin.site.register(models.DriverSurvey)
-admin.site.register(models.OwnerSurvey)
+class ContactAdmin(admin.ModelAdmin):
+    fields = [
+        'email',
+        'role_name',
+        'zipcode',
+        'created_time',
+    ]
+    list_display = [
+        'email',
+        'role_name',
+        'zipcode',
+        'created_time',
+    ]
+    readonly_fields = [
+        'email',
+        'role_name',
+        'zipcode',
+        'created_time',
+    ]
+
+    def role_name(self, instance):
+        return unicode(instance.role)
+
+admin.site.register(models.Contact, ContactAdmin)
