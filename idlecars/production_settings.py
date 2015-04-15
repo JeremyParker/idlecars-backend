e8db975395116008eb91e85b597e20e16ae65727
jefk <speedracer111@gmail.com>
2015-04-15 14:04:59 -0400

    env specific settings

---
+++ idlecars/production_settings.py
@@ -0,0 +1,10 @@
+# -*- encoding:utf-8 -*-
+from __future__ import unicode_literals
+
+from settings import *
+
+# heap analytics tracking for production
+HEAP_APP_ID = '3053705704'
+
+# Allow cross origin requests from these domains
+CORS_ORIGIN_WHITELIST = ('app.idlecars.com'),)
