e52c26d7f9493ad7d122071a185dfad813659281
Jeremy Parker <github@jeremyparker.org>
2015-04-15 14:23:57 -0400

    make the cars list api use the same fields as the client expects.

--- server/serializers.py
+++ server/serializers.py
@@ -7,16 +7,33 @@ from models import Car
 
 
 class CarSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField()
+    listing_features = serializers.SerializerMethodField()
+    cost = serializers.SerializerMethodField()
+    cost_time = serializers.SerializerMethodField()
+
     class Meta:
         model = Car
         fields = (
             'id',
-            'next_available_date',
-            'year',
-            'solo_cost',
-            'solo_deposit',
-            'split_cost',
-            'split_deposit',
-            'min_lease',
+            'name',
+            'listing_features',
+            'cost',
+            'cost_time',
         )
-        depth = 1
+
+    def get_name(self, obj):
+        return unicode(obj.make_model)
+
+    def get_listing_features(self, obj):
+        return '{} minimum lease, {}, {}, Idlecars Certified'.format(
+            obj.min_lease,
+            obj.owner.city,
+            obj.owner.state_code
+        )
+
+    def get_cost(self, obj):
+        return unicode(obj.solo_cost)
+
+    def get_cost_time(self, obj):
+        return 'a week'
