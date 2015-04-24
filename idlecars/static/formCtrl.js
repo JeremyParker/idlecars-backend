0b8b7b1cf4039d6cd2c726561f4f6c67b4c3bab2
craigstar <Man@MantekiMacBook-Pro.local>
2015-04-24 11:31:00 -0400

    translated tabs to spaces

--- idlecars/static/formCtrl.js
+++ idlecars/static/formCtrl.js
@@ -7,53 +7,53 @@ app.controller('formCtrl', function($scope){
 
   $scope.validation = function(event){
 
-	$scope.validateZipCode();
-	$scope.validateEmail();
-  } 
+  $scope.validateZipCode();
+  $scope.validateEmail();
+  }
 
   $scope.validateZipCode = function(){
 
-		var zipcode_required = $scope.myForm.zipcode.$error.required;
-		var zipcode_pattern = $scope.myForm.zipcode.$error.pattern;
+    var zipcode_required = $scope.myForm.zipcode.$error.required;
+    var zipcode_pattern = $scope.myForm.zipcode.$error.pattern;
 
-		// zip code input validation
-		if (zipcode_required) {
-			$scope.zipLabel = 'Zipcode is required';
-			$scope.zipRed = true;
-			event.preventDefault();
-			
-		}
-		else if (zipcode_pattern) {
-			$scope.zipLabel = 'Please enter a valid zip code';
-			$scope.zipRed = true;
-			event.preventDefault();
-		}
-		else{
-			$scope.zipLabel = 'Zipcode:';
-			$scope.zipRed = false;
-		};
-	}
+    // zip code input validation
+    if (zipcode_required) {
+      $scope.zipLabel = 'Zipcode is required';
+      $scope.zipRed = true;
+      event.preventDefault();
 
-	$scope.validateEmail = function(){
+    }
+    else if (zipcode_pattern) {
+      $scope.zipLabel = 'Please enter a valid zip code';
+      $scope.zipRed = true;
+      event.preventDefault();
+    }
+    else{
+      $scope.zipLabel = 'Zipcode:';
+      $scope.zipRed = false;
+    };
+  }
 
-		var email_required = $scope.myForm.email.$error.required;
-		var email_invalid = $scope.myForm.email.$invalid;
+    $scope.validateEmail = function(){
 
-		// email input validation
-		if (email_required) {
-			$scope.emailLabel = 'Email is required';
-			$scope.emailRed = true;
-			event.preventDefault();
-		}
-		else if (email_invalid) {
-			$scope.emailLabel = 'Please enter a valid email';
-			$scope.emailRed = true;
-			event.preventDefault();
-		}
-		else{
-			$scope.emailLabel = 'Email:';
-			$scope.emailRed = false;
-		};
-	}
-	
+    var email_required = $scope.myForm.email.$error.required;
+    var email_invalid = $scope.myForm.email.$invalid;
+
+    // email input validation
+    if (email_required) {
+      $scope.emailLabel = 'Email is required';
+      $scope.emailRed = true;
+      event.preventDefault();
+    }
+    else if (email_invalid) {
+      $scope.emailLabel = 'Please enter a valid email';
+      $scope.emailRed = true;
+      event.preventDefault();
+    }
+    else{
+      $scope.emailLabel = 'Email:';
+      $scope.emailRed = false;
+    };
+  }
+
 })
