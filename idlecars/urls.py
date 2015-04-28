216339a9ec8e5163c87cb5abf46901527dc77f21
Jeremy Parker <github@jeremyparker.org>
2015-04-28 15:02:30 -0400

    make browsable root completely gone if ApiBrowserRenderer is inactive

--- idlecars/urls.py
+++ idlecars/urls.py
@@ -9,7 +9,6 @@ import server.views
 urlpatterns = patterns('',
     url(r'', include('website.urls', namespace='website')),
 
-    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/', include('server.urls', namespace='server')),
 
     url(r'^admin/', include(admin.site.urls)),
