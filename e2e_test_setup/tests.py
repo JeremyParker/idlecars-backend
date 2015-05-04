e50ec2546cbf9f3cc72a0bd5e3823b97a427c9be
jefk <speedracer111@gmail.com>
2015-05-04 15:51:01 -0400

    add explanation

--- e2e_test_setup/tests.py
+++ e2e_test_setup/tests.py
@@ -6,5 +6,9 @@ from rest_framework import status
 
 class ExistsTest(TestCase):
     def test_endpoint_doesnt_exist(self):
+        '''
+            This test ensures that you cannot access this endpoint
+            It is only allowed when `idlecars.test_settings` are invoked
+        '''
         response = self.client.get('/e2e_test_setup')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
