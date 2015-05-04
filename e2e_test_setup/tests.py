a854bcfb8228ef3ebed4fee7e3e7e6b2fcc9358d
jefk <speedracer111@gmail.com>
2015-05-04 15:49:40 -0400

    add 404 test

--- e2e_test_setup/tests.py
+++ e2e_test_setup/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+# -*- encoding:utf-8 -*-
+from __future__ import unicode_literals
 
-# Create your tests here.
+from django.test import TestCase
+from rest_framework import status
+
+class ExistsTest(TestCase):
+    def test_endpoint_doesnt_exist(self):
+        response = self.client.get('/e2e_test_setup')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
