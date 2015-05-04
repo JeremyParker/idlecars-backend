19f81bea11634d05ecb40a276700d96bffd85836
jefk <speedracer111@gmail.com>
2015-05-04 15:16:47 -0400

    add app for setting up e2e tests

---
+++ idlecars/test_settings.py
@@ -0,0 +1,31 @@
+# -*- encoding:utf-8 -*-
+from __future__ import unicode_literals
+
+from settings import *
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "idlecars_test",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "",
+    }
+}
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+WEBAPP_URL = 'localhost:3000'
+
+# Allow cross origin requests from these domains
+CORS_ORIGIN_WHITELIST = ('localhost:3000',)
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
+
+WARNING__ENABLE_TEST_SETUP_ENDPOINT__TEST_MODE_ONLY = True
--- idlecars/urls.py
+++ idlecars/urls.py
@@ -12,4 +12,6 @@ urlpatterns = patterns('',
     url(r'^api/', include('server.urls', namespace='server')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'', include('e2e_test_setup.urls', namespace='e2e_test_setup')),
 )
