daa9f17982fd340c71a8f7878037f24c86b7b90f
jefk <speedracer111@gmail.com>
2015-05-04 18:18:18 -0400

    add cars

--- e2e_test_setup/views.py
+++ e2e_test_setup/views.py
@@ -4,9 +4,11 @@ from __future__ import unicode_literals
 from django.http import JsonResponse
 from django.conf import settings
 
+import models
+
 def e2e_test_setup(request):
     if settings.WARNING__ENABLE_TEST_SETUP_ENDPOINT__TEST_MODE_ONLY:
-        # NOTE: do the bad stuff here
+        models.E2ETestSetup().perform()
         return JsonResponse({})
     else:
         return JsonResponse({}, status_code=400)
