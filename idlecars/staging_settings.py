c7b64f348c1d7e2b6ab9019dcdccd1e26aa42bf9
jefk <speedracer111@gmail.com>
2015-05-07 10:56:17 -0400

    limit hosts on staging

--- idlecars/staging_settings.py
+++ idlecars/staging_settings.py
@@ -8,6 +8,8 @@ HEAP_APP_ID = '1900221263'
 
 WEBAPP_URL = 'app.staging.idlecars.com'
 
+ALLOWED_HOSTS = ['staging.idlecars.com']
+
 # Allow cross origin requests from these domains
 CORS_ORIGIN_WHITELIST = (
     'app.staging.idlecars.com.s3-website-us-east-1.amazonaws.com',
