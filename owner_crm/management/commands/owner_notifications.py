e15ac3dd0cfdeed3fc39912b2bb18fd1035b9ac8
Jeremy Parker <github@jeremyparker.org>
2015-05-21 17:01:01 +0100

    use renewal_url service

--- owner_crm/management/commands/owner_notifications.py
+++ owner_crm/management/commands/owner_notifications.py
@@ -6,7 +6,7 @@ from django.template import Context
 from django.template.loader import render_to_string
 from django.conf import settings
 
-from idlecars import email
+from idlecars import email, client_side_routes
 from server.services import car as car_service
 from owner_crm.models import Renewal
 
@@ -24,14 +24,7 @@ class Command(BaseCommand):
         for car in notifiable_cars:
             for user in car.owner.user_account.all():
                 renewal = Renewal.objects.create(car=car)
-
-                # TODO - use renewal_url service
-                renewal_url = 'http://{app_url}/#/cars/{car_id}/renewals/{renewal_token}'.format(
-                    app_url=settings.WEBAPP_URL,
-                    car_id=car.id,
-                    renewal_token=renewal.token,
-                )
-
+                renewal_url = client_side_routes.renewal_url(renewal)
                 body = self.render_body(car)
                 merge_vars = {
                     user.email: {
@@ -44,9 +37,9 @@ class Command(BaseCommand):
                 email.send_async('single_cta', 'Your idlecars listing is about to expire.', merge_vars)
 
     def render_body(self, car):
-                template_data = {
-                    'CAR_NAME': car.__unicode__(),
-                    'CAR_PLATE': car.plate,
-                }
-                context = Context(autoescape=False)
-                return render_to_string("car_expiring.jade", template_data, context)
+        template_data = {
+            'CAR_NAME': car.__unicode__(),
+            'CAR_PLATE': car.plate,
+        }
+        context = Context(autoescape=False)
+        return render_to_string("car_expiring.jade", template_data, context)
