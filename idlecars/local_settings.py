64aedc62daed0e14377180416c266287a22032e0
jefk <speedracer111@gmail.com>
2015-05-21 12:01:33 -0400

    make sure the a has an href

--- idlecars/local_settings.py
+++ idlecars/local_settings.py
@@ -22,7 +22,7 @@ SSLIFY_DISABLE = True
 # Heap Analytics uses the DEBUG app id in development/testing
 HEAP_APP_ID = '655181858'
 
-WEBAPP_URL = 'localhost:3000'
+WEBAPP_URL = 'http://localhost:3000'
 
 # Allow cross origin requests from these domains
 CORS_ORIGIN_WHITELIST = ('localhost:3000',)
