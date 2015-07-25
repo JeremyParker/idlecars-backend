04cd06ecd6f157d19f973c8e49c05d3b24d3639b
jefk <speedracer111@gmail.com>
2015-07-24 18:21:45 -0700

    add friendly id to rideshare provider

--- server/models/rideshare_provider.py
+++ server/models/rideshare_provider.py
@@ -8,6 +8,7 @@ from server.models import MakeModel
 
 class RideshareProvider(models.Model):
     name = models.CharField(max_length=128, blank=False)
+    frieldly_id = models.CharField(max_length=32, blank=False)
     compatible_models = models.ManyToManyField(MakeModel)
 
     def __unicode__(self):
