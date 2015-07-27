1d943b2dfa64159a2efcc2ddfedfc05d387917bd
jefk <speedracer111@gmail.com>
2015-07-26 21:59:46 -0700

    add test for non compatibility

--- server/tests/test_models/test_car_compatibility.py
+++ server/tests/test_models/test_car_compatibility.py
@@ -8,6 +8,7 @@ from server.models import CarCompatibility
 
 class TestCarCompatibility(TestCase):
     def setUp(self):
+        self.non_compaitble = MakeModel.create(make='BMW', model='i3')
         self.delorean_2 = MakeModel.create(make='DMC', model='Delorean 2')
 
         self.uber_x = RideshareProviderFactory.create(name='uberX', friendly_id='uber_x')
@@ -19,4 +20,8 @@ class TestCarCompatibility(TestCase):
 
     def test_old_car(self):
         car = Car.create(year=1985, make_model=self.delorean_2)
+        self.assertFalse(CarCompatibility(car).uber_x())
+
+    def test_non_compatible_model(self):
+        car = Car.create(year=2021, make_model=self.non_compaitble)
         self.assertFalse(CarCompatibility(car).uber_x())
