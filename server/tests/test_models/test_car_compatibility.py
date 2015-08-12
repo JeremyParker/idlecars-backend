7341b197d84610b5ce746774c2d357243a912f84
jefk <speedracer111@gmail.com>
2015-08-12 16:15:37 -0700

    add some tests

--- server/tests/test_models/test_car_compatibility.py
+++ server/tests/test_models/test_car_compatibility.py
@@ -22,17 +22,35 @@ class TestComaptibleMakeModel(TestCase):
         car = Car.create(make_model=self.non_compaitble)
         self.assertFalse(CarCompatibility(car).uber_x())
 
-# class TestLyft(TestCase):
-#     def setUp(self):
-#         self.lyft_standard = RideshareFlavorFactory.create(name='Lyft', friendly_id='lyft_standard')
-#         self.lyft_plus = RideshareFlavorFactory.create(name='Lyft Plus', friendly_id='lyft_plus')
 
-#     def test_compatible_model(self):
-#         mm = MakeModel.create(passenger_count=42)
-#         car = Car.create(make_model=mm)
-#         self.assertEqual(self.lyft_standard.name, CarCompatibility(car).uber_x())
+class TestLyft(TestCase):
+    def setUp(self):
+        self.lyft_standard = RideshareFlavorFactory.create(name='Lyft', friendly_id='lyft_standard')
+        self.lyft_plus = RideshareFlavorFactory.create(name='Lyft Plus', friendly_id='lyft_plus')
 
-#     def test_non_compatible_model(self):
-#         mm = MakeModel.create(passenger_count=42)
-#         car = Car.create(make_model=mm)
-#         self.assertEqual(self.lyft_plus.name, CarCompatibility(car).uber_x())
+    def test_lil_car(self):
+        mm = MakeModel.create(passenger_count=2)
+        car = Car.create(make_model=mm)
+        self.assertEqual(self.lyft_standard.name, CarCompatibility(car).lyft_standard())
+
+    def test_big_car(self):
+        mm = MakeModel.create(passenger_count=22)
+        car = Car.create(make_model=mm)
+        self.assertEqual(self.lyft_plus.name, CarCompatibility(car).lyft_plus())
+
+
+class TestGett(TestCase):
+    def setUp(self):
+        self.gett_standard = RideshareFlavorFactory.create(name='Gett', friendly_id='gett_standard')
+
+    def test_black_on_black(self):
+        car = Car.create(exterior_color=0, interior_color=0)
+        self.assertEqual(self.gett_standard.name, CarCompatibility(car).gett_standard())
+
+    def test_black_on_blue(self):
+        car = Car.create(exterior_color=0, interior_color=3)
+        self.assertFalse(CarCompatibility(car).gett_standard())
+
+    def test_blue_on_black(self):
+        car = Car.create(exterior_color=4, interior_color=0)
+        self.assertFalse(CarCompatibility(car).gett_standard())
