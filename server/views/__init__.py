59c197d4f2d14ebdab8008e10acb4866ffdce2d9
Jeremy Parker <github@jeremyparker.org>
2015-08-12 23:37:39 -0400

    cleaning up PR more

--- server/views/__init__.py
+++ server/views/__init__.py
@@ -2,7 +2,6 @@
 from __future__ import unicode_literals
 
 from booking_view import BookingViewSet
-from cancelation_view import CancelationView
 from car_view import CarViewSet
 from driver_view import DriverViewSet
 from phone_number_view import PhoneNumberDetailView
