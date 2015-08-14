9f9c496d40cd15b3f3dbe97201b4b20daf8d2ba9
Jeremy Parker <github@jeremyparker.org>
2015-08-14 11:25:42 -0400

    api sends a formatted phone number for owners

--- server/serializers/owner_serializer.py
+++ server/serializers/owner_serializer.py
@@ -3,11 +3,13 @@ from __future__ import unicode_literals
 
 from rest_framework import serializers
 
+from idlecars import fields
 from server.models import Owner
 
 
 class OwnerSerializer(serializers.ModelSerializer):
     address = serializers.SerializerMethodField()
+    phone_number = fields.PhoneNumberField(max_length=30)
 
     class Meta:
         model = Owner
