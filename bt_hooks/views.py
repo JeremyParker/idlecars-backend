43cc4d6ccf459a03141f2cad9199651d2d66bfb9
jefk <speedracer111@gmail.com>
2015-08-25 16:09:32 -0700

    right view file

--- bt_hooks/views.py
+++ bt_hooks/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# -*- encoding:utf-8 -*-
+from __future__ import unicode_literals
 
-# Create your views here.
+import braintree
+
+
+def submerchant_create_success(request):
+    if request.method == "GET":
+        return braintree.WebhookNotification.verify(request.args['bt_challenge'])
+
+def submerchant_create_failure(request):
+    if request.method == "GET":
+        return braintree.WebhookNotification.verify(request.args['bt_challenge'])
