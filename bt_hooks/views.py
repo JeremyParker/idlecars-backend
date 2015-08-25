ac39004594a271a2adce72e1cbd571a00f50b09c
jefk <speedracer111@gmail.com>
2015-08-25 16:24:52 -0700

    configure bt

--- bt_hooks/views.py
+++ bt_hooks/views.py
@@ -4,11 +4,19 @@ from __future__ import unicode_literals
 import braintree
 
 
+def _register_bt_endpoint(request):
+    braintree.Configuration.configure(
+        braintree.Environment.Sandbox,
+        'cg5tqqwr6fn5xycb',
+        '7vyzyb772bwnhj3x',
+        '951f45c0a8bf94e474b8eb5e956402fd'
+    )
+    return braintree.WebhookNotification.verify(request.GET['bt_challenge'])
+
 def submerchant_create_success(request):
     if request.method == "GET":
-        return braintree.WebhookNotification.verify(request.GET['bt_challenge'])
+        return _register_bt_endpoint(request)
 
 def submerchant_create_failure(request):
-    print request
     if request.method == "GET":
-        return braintree.WebhookNotification.verify(request.GET['bt_challenge'])
+        return _register_bt_endpoint(request)
