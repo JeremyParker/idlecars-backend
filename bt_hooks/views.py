82dacabfef94c06cb9d7df19b92fcd606a591ae6
jefk <speedracer111@gmail.com>
2015-08-26 09:06:08 -0700

    return text response

--- bt_hooks/views.py
+++ bt_hooks/views.py
@@ -1,8 +1,9 @@
 # -*- encoding:utf-8 -*-
 from __future__ import unicode_literals
 
-import braintree
+from django.http import HttpResponse
 
+import braintree
 
 def _register_bt_endpoint(request):
     braintree.Configuration.configure(
@@ -11,7 +12,8 @@ def _register_bt_endpoint(request):
         '7vyzyb772bwnhj3x',
         '951f45c0a8bf94e474b8eb5e956402fd'
     )
-    return braintree.WebhookNotification.verify(request.GET['bt_challenge'])
+    reply = braintree.WebhookNotification.verify(request.GET['bt_challenge'])
+    return HttpResponse(reply, content_type='text/plain')
 
 def submerchant_create_success(request):
     if request.method == "GET":
