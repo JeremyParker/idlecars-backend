1100a5f1da0a689f762b1904d0e5e9b934263a97
jefk <speedracer111@gmail.com>
2015-08-26 09:35:40 -0700

    send an email

--- bt_hooks/views.py
+++ bt_hooks/views.py
@@ -15,10 +15,33 @@ def _register_bt_endpoint(request):
     reply = braintree.WebhookNotification.verify(request.GET['bt_challenge'])
     return HttpResponse(reply, content_type='text/plain')
 
+def _send_email_to_admin(request):
+    merge_vars = {
+        'jeff@idlecars.com': {
+            'FNAME': 'Admin',
+            'HEADLINE': 'Owner Bank Link Update',
+            'TEXT': request.POST.__unicode__(),
+            'CTA_LABEL': 'Dont click me',
+            'CTA_URL': 'https://idlecars.com',
+        }
+    }
+    email.send_async(
+        template_name='one_button_no_image',
+        subject='Owner Bank Link Update',
+        merge_vars=merge_vars,
+    )
+
+
 def submerchant_create_success(request):
-    if request.method == "GET":
+    if request.method == 'GET':
         return _register_bt_endpoint(request)
+    elif request.method == 'POST':
+        _send_email_to_admin(request)
+        return HttpResponse('')
 
 def submerchant_create_failure(request):
-    if request.method == "GET":
+    if request.method == 'GET':
         return _register_bt_endpoint(request)
+    elif request.method == 'POST':
+        _send_email_to_admin(request)
+        return HttpResponse('')
