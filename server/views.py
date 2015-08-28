6f275018f44eabc61a0206bc81d5222f23883294
Jeremy Parker <github@jeremyparker.org>
2015-08-28 14:03:31 -0400

    move OwnerViewSet back to the communal file that we use on master :(

--- server/views.py
+++ server/views.py
@@ -7,6 +7,8 @@ from rest_framework.views import APIView
 from rest_framework import viewsets, mixins
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.decorators import detail_route
+from rest_framework import status
 
 from idlecars import fields
 
@@ -14,6 +16,9 @@ import models, services
 from serializers import CarSerializer, BookingSerializer, BookingDetailsSerializer
 from serializers import DriverSerializer, PhoneNumberSerializer
 from permissions import OwnsDriver, OwnsBooking
+from server.models import Owner
+from server.services import owner_bank_link_service
+from server.permissions import OwnsOwner
 
 
 class CarViewSet(viewsets.ReadOnlyModelViewSet):
@@ -67,6 +72,23 @@ class DriverViewSet(
         if self.request.user.is_authenticated() and self.kwargs[lookup_url_kwarg] == 'me':
             self.kwargs[lookup_url_kwarg] = models.Driver.objects.get(auth_user=self.request.user).pk
         return super(DriverViewSet, self).get_object()
+
+
+class OwnerViewSet(
+        mixins.RetrieveModelMixin,
+        viewsets.GenericViewSet
+    ):
+    queryset = Owner.objects.all()
+
+    @detail_route(methods=['post'], permission_classes=[OwnsOwner])
+    def bank_link(self, request, pk=None):
+        owner = self.get_object()
+        result = owner_bank_link_service.link(owner, request.data)
+
+        if not result.get('error'):
+            return Response(result, status=status.HTTP_201_CREATED)
+        else:
+            return Response(result, status=status.HTTP_400_BAD_REQUEST)
 
 
 class PhoneNumberDetailView(APIView):
