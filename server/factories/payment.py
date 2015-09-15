17a3a5a39b83d5010e562718de9ce07a5fbc77e6
Jeremy Parker <github@jeremyparker.org>
2015-09-15 16:39:51 -0400

    make a factory for payments

---
+++ server/factories/payment.py
@@ -0,0 +1,20 @@
+# -*- encoding:utf-8 -*-
+from __future__ import unicode_literals
+
+from random import randint
+from decimal import Decimal
+
+from idlecars.factory_helpers import Factory
+
+from server import models
+
+
+class Payment(Factory):
+    class Meta:
+        model = 'server.Payment'
+    amount = Decimal('9.{}'.format(randint(1, 99)))
+
+
+class PreAuthorizedPayment(Payment):
+    transaction_id = 'some_transaction_id'
+    status = models.Payment.PRE_AUTHORIZED
