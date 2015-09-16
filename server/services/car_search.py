715311cebed2d35641760802ab00097b04a852e9
craigstar <craig@idlecars.com>
2015-09-16 12:00:27 -0400

    add work with array in serializer

--- server/services/car_search.py
+++ server/services/car_search.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from server import models
+from server.models import CarCompatibility
 
 def get_cost_bucket(car):
     if car.normalized_cost() < 60:
@@ -17,6 +18,7 @@ def search_attrs(car):
         'cost_bucket': get_cost_bucket(car),
         'body_type': _body_type(car),
         'lux_level': _lux_level(car),
+        'work_with': _work_with(car),
     }
 
 def _body_type(car):
@@ -24,3 +26,6 @@ def _body_type(car):
 
 def _lux_level(car):
     return 'Standard' if car.make_model.lux_level == 0 else 'Luxury'
+
+def _work_with(car):
+    return CarCompatibility(car).all()
