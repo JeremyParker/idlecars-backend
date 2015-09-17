abd01c6d742f176aee5150c791e5105015426e3f
craigstar <craig@idlecars.com>
2015-09-17 12:23:54 -0400

    searchable values should be an array

--- server/services/car_search.py
+++ server/services/car_search.py
@@ -6,11 +6,11 @@ from server.models import CarCompatibility
 
 def get_cost_bucket(car):
     if car.normalized_cost() < 60:
-        return 'cheap'
+        return ['cheap']
     elif car.normalized_cost() < 80:
-        return 'medium'
+        return ['medium']
     else:
-        return 'pricey'
+        return ['pricey']
 
 
 def search_attrs(car):
@@ -22,10 +22,10 @@ def search_attrs(car):
     }
 
 def _body_type(car):
-    return 'Sedan' if car.make_model.body_type == 0 else 'SUV'
+    return ['Sedan'] if car.make_model.body_type == 0 else ['SUV']
 
 def _lux_level(car):
-    return 'Standard' if car.make_model.lux_level == 0 else 'Luxury'
+    return ['Standard'] if car.make_model.lux_level == 0 else ['Luxury']
 
 def _work_with(car):
     return CarCompatibility(car).all()
