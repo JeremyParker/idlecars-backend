96601a394edc6b50cbcdc3ae5787254d09ff59a4
Jeremy Parker <github@jeremyparker.org>
2015-10-05 18:02:27 -0400

    fix merge issues

--- server/views/owner_phone_number_view.py
+++ server/views/owner_phone_number_view.py
@@ -1,6 +1,7 @@
 # -*- encoding:utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -18,7 +19,7 @@ class OwnerPhoneNumberDetailView(APIView):
         phone_number = fields.parse_phone_number(pk)
         try:
             auth_user = User.objects.get(username=phone_number)
-            owner = models.Owner.objects.get(auth_users=auth_user)
+            owner = Owner.objects.get(auth_users=auth_user)
         except (Owner.DoesNotExist, User.DoesNotExist):
             return Response('', status.HTTP_404_NOT_FOUND)
 
--- server/views/phone_number_view.py
+++ server/views/phone_number_view.py
@@ -5,6 +5,7 @@ from django.http import Http404
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from idlecars import fields
 
