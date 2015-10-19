8f267569e75496d63f4fdd15bae06c0a7eb161af
Jeremy Parker <github@jeremyparker.org>
2015-10-19 15:56:45 -0400

    test that we use the latest payment_method

--- server/tests/test_services/test_payment_service.py
+++ server/tests/test_services/test_payment_service.py
@@ -6,19 +6,33 @@ from django.utils import timezone
 
 from server.factories import CompletedDriver, Booking
 from server.services import payment as payment_service
+from server.services import driver as driver_service
+from server.services import payment_method as payment_method_service
+
 
 class PaymentServiceTest(TestCase):
     def setUp(self):
-        pass
+        self.driver = CompletedDriver.create()
+        self.booking = Booking.create(driver=self.driver)
 
     def test_create_payment_with_no_payment_method(self):
-        driver = CompletedDriver.create()
-        booking = Booking.create(driver=driver)
         payment = payment_service.create_payment(
-            booking,
+            self.booking,
             amount = '66.66',
             service_fee = '0.00',
             invoice_start_time = timezone.now(),
             invoice_end_time = timezone.now(),
         )
         self.assertIsNotNone(payment.error_message)
+
+    def test_create_payment_with_multiple_payment_method(self):
+        # add a payment method
+        payment_method_service.add_payment_method(self.driver, 'some_nonce')
+        first_payment_method = driver_service.get_default_payment_method(self.driver)
+
+        # add a second payment method
+        payment_method_service.add_payment_method(self.driver, 'some_other_nonce')
+        new_payment_method = driver_service.get_default_payment_method(self.driver)
+
+        # the new_payment_method should not be the first payment method.
+        self.assertNotEqual(first_payment_method, new_payment_method)
