b2c71255e087b95dcd173d698165e2f37f77591b
craigstar <craig@idlecars.com>
2015-10-20 19:10:33 -0400

    js setup

--- idlecars/static/javascript/about.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('app', [])
-.controller('about.controller', function ($scope) {
-
-});
---
+++ idlecars/static/javascript/aboutCtrl.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('app', [])
+.controller('aboutCtrl', function ($scope) {
+  console.log($scope.selection)
+});
