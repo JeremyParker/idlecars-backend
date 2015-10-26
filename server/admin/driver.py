cd66aaafcc9e6425db5b5136d9ca47ba05c86871
Jeremy Parker <github@jeremyparker.org>
2015-10-26 10:32:33 -0400

    remove unused import

--- server/admin/driver.py
+++ server/admin/driver.py
@@ -4,7 +4,6 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from django.contrib import auth
 
-from idlecars.admin_helpers import link
 from idlecars.reverse_admin import ReverseModelAdmin
 
 from server import models
