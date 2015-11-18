2ceefcfd72f29b2bdec80feb65b4cef13ec93a18
Jeremy Parker <github@jeremyparker.org>
2015-11-18 12:09:44 -0500

    add another unit test

--- server/tests/test_api/test_user.py
+++ server/tests/test_api/test_user.py
@@ -49,6 +49,12 @@ class UserApiTest(APITestCase):
         user_reloaded = User.objects.get(pk=self.user.pk)
         self.assertEqual(user_reloaded.first_name, 'Mikey')
 
+    def test_update_last_name(self):
+        response = self.client.patch(self.url, {'last_name': 'McCarthy'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        user_reloaded = User.objects.get(pk=self.user.pk)
+        self.assertEqual(user_reloaded.last_name, 'McCarthy')
+
     def test_cannot_update_someone_else(self):
         other_user = factories.AuthUser.create()
         url = reverse('server:users-detail', args=(other_user.id,))
