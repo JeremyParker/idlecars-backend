290884db0127325d0c941dfb01ef3022373e4cc8
craigstar <craig@idlecars.com>
2015-12-09 17:56:19 -0500

    redirect

--- website/urls.py
+++ website/urls.py
@@ -10,4 +10,5 @@ urlpatterns = patterns('',
     url(r'^about$', views.about, name='about'),
     url(r'^contact/(\d+)/driver_survey/new$', views.driver_survey, name='driver_survey'),
     url(r'^contact/(\d+)/owner_survey/new$', views.owner_survey, name='owner_survey'),
+    url(r'^(?P<credit>[0-9]{2})$', views.driver_referral, name='driver_referral'),
 )
