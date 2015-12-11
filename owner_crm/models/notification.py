3a13037d2fd07c808353c12810a49f1b15de1929
craigstar <craig@idlecars.com>
2015-12-11 18:00:18 -0500

    next_credit_amount, InvitorReceivedCredit

--- owner_crm/models/notification.py
+++ owner_crm/models/notification.py
@@ -62,6 +62,7 @@ def _get_booking_params(booking):
             reverse('admin:server_booking_change', args=(booking.pk,))
         ),
         'booking_next_payment_amount': next_payment_amount,
+        'booking_next_credit_amount': next_credit_amount,
         'booking_payment_fee': booking_payment_fee,
         'booking_invoice_start_time': invoice_start_time,
         'booking_invoice_end_time': invoice_end_time,
