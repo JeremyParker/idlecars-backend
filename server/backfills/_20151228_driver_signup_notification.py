9ba02afbae8ab8c329988d76fa9c0206a14fbc7b
craigstar <craig@idlecars.com>
2015-12-28 13:34:14 -0500

    signup backfill

---
+++ server/backfills/_20151228_driver_signup_notification.py
@@ -0,0 +1,24 @@
+# -*- encoding:utf-8 -*-
+from __future__ import unicode_literals
+
+from server import models, services
+
+def run_backfill():
+    '''
+    mark throttle system the existing drivers who signed up 29 days ago FirstSignupReminder so that
+    they don't get two reminders at the same time when we launch
+    '''
+
+    backfill_drivers = models.Driver.objects.filter(
+        auth_user__date_joined__lte=timezone.now() - datetime.timedelta(days=29),
+        braintree_customer_id__isnull=True,
+    )
+    skip_drivers = []
+    for driver in backfill_drivers:
+        if services.booking.post_pending_bookings(driver.booking_set):
+            skip_drivers.append(driver.pk)
+            continue
+    throttle_service.mark_sent(backfill_drivers.exclude(pk__in=skip_drivers), 'SignupFirstReminder')
+
+
+
