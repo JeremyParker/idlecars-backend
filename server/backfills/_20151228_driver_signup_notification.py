79974f489758168a8d5cf227bfa6a6167818a3e1
craigstar <craig@idlecars.com>
2015-12-28 13:55:44 -0500

    declaration

--- server/backfills/_20151228_driver_signup_notification.py
+++ server/backfills/_20151228_driver_signup_notification.py
@@ -1,6 +1,9 @@
 # -*- encoding:utf-8 -*-
 from __future__ import unicode_literals
 
+import datetime
+from django.utils import timezone
+
 from server import models, services
 from owner_crm.services import throttle_service
 
