673b9f56fbd612c831edf953980433aeac091165
Jeremy Parker <github@jeremyparker.org>
2015-12-31 12:45:44 -0500

    message record admin tweak

--- owner_crm/admin/message.py
+++ owner_crm/admin/message.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from idlecars.admin_helpers import link
+
 
 class MessageAdmin(admin.ModelAdmin):
     list_display = [
@@ -10,6 +12,13 @@ class MessageAdmin(admin.ModelAdmin):
         'campaign',
         'owner',
         'car',
-        'driver',
+        'driver_link',
         'booking',
     ]
+
+    def driver_link(self, instance):
+        if instance.driver:
+            return link(instance.driver, instance.driver.admin_display())
+        else:
+            return None
+    driver_link.short_description = 'Driver'
