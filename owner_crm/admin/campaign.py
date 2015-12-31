b4a75209adf4a35094e06301bab16417da1d61ad
Jeremy Parker <github@jeremyparker.org>
2015-12-31 11:19:40 -0500

    backfills can be run multiple times

--- owner_crm/admin/campaign.py
+++ owner_crm/admin/campaign.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 
-class Campaign(admin.ModelAdmin):
+class CampaignAdmin(admin.ModelAdmin):
     fields = [
         'name',
         'preferred_medium',
